78ad494ae1e54231db0a63e967ae2a157e6421b8
asadaqain <sadaqain@yorku.ca>
2018-02-05 14:44:38 -0500

    added headless to application_system_test_case.rb and cleaned up missing items report test

--- test/system/missing_items_report_test.rb
+++ test/system/missing_items_report_test.rb
@@ -1,18 +1,10 @@
 require "application_system_test_case"
 
 class MissingItemsReportTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit missing_items_report_index_url
-  #
-  #   assert_selector "h1", text: "MissingItemsReport"
-  # end
-  
+    
   test "load the index page" do
-    # visit root_url
-    get root_url
-    # visit missing_items_report_index_path
-    # assert_selector "h1", text: "List of Missing Items: Report"
-    assert_selector "h1", text: "Dashboard"
+    visit missing_items_report_index_path
+    assert_selector "h1", text: "List of Missing Items: Report"
   end
   
 end
