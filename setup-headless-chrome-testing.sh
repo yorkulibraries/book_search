88084d0d0b56c067203c509ef5f778ad3dbdf328
Tuan Nguyen <amtuannguyen@gmail.com>
2018-02-05 14:11:51 -0500

    missing unzip

--- setup-headless-chrome-testing.sh
+++ setup-headless-chrome-testing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-sudo apt install -y xvfb
+cd ~
+sudo apt install -y xvfb unzip
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb 
