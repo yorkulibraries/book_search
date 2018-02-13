5a8e6d0dc00da7486e4edd3424d40a55ac3b50c5
Taras Danylak <taras@danylak.com>
2018-02-13 09:14:10 -0500

    Spelling mistake

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -2,20 +2,23 @@ require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
 
-  should "create a valid Patron" do
-    assert_difference "Patron.count", 1 do
-      patron = build(:patron)
-      patron.save
+  should "create a valid Employee" do
+    assert_difference "Employee.count", 1 do
+      employee = build(:employee)
+      employee.save
     end
   end
 
-  should "not create an invalid patron" do
-    assert ! build(:patron, email: nil).valid?, "Email Address is required"
-    assert ! build(:patron, name: nil).valid?, "Name is required"
-    assert ! build(:patron, login_id: nil).valid?, "Login ID is required"
+  should "not create an invalid Employee" do
+    assert ! build(:employee, email: nil).valid?, "Email Address is required"
+    assert ! build(:employee, name: nil).valid?, "Name is required"
+    assert ! build(:employee, login_id: nil).valid?, "Login ID is required"
+    assert ! build(:employee, role: nil).valid?, "Role is required"
+    # assert ! build(:employee, location: nil).valid?, "Location is required"
 
-    patron = create(:patron)
-    assert ! build(:patron, login_id: patron.login_id).valid?, "Should be invalid since patron with login is created already"
+    employee = create(:employee)
+    assert ! build(:employee, login_id: employee.login_id).valid?, "Should be invalid since patron with login is created already"
+
   end
 
 
