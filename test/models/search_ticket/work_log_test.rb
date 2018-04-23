2f3cc1f6b13db9f1d429be6dddbbb81e128456da
Taras Danylak <taras@danylak.com>
2018-04-23 12:27:06 -0400

    Working on recording search work

--- test/models/search_ticket/work_log_test.rb
+++ test/models/search_ticket/work_log_test.rb
@@ -3,20 +3,23 @@ require 'test_helper'
 class SearchTicket::WorkLogTest < ActiveSupport::TestCase
   should "create a valid WorkLog" do
     assert_difference "SearchTicket::WorkLog.count", 1 do
-      sa = build(:work_log)
-      sa.save
+      log = build(:work_log)
+      log.save
     end
   end
 
   should "not create an invalid WorkLog" do
     assert ! build(:work_log, search_ticket_id: nil).valid?, "Item ID is required"
     assert ! build(:work_log, employee: nil).valid?, "Employee is required"
-
+    assert ! build(:work_log, work_type: nil).valid?, "Work Type is required"
+    assert ! build(:work_log, result: nil).valid?, "Result is required"
+    assert ! build(:work_log, result:  SearchTicket::WorkLog::RESULT_FOUND, found_location: nil).valid?, "Found location is required, if result is found"
   end
 
   should "set resolution on create to default values" do
-    sa = build(:work_log)
-    sa.save
-    assert_equal SearchTicket::WorkLog::RESULT_UNKNOWN, sa.result
+    log = build(:work_log)
+    log.save
+    assert_equal SearchTicket::WorkLog::RESULT_UNKNOWN, log.result
   end
+
 end
