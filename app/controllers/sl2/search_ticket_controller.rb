62d87440620eeaadbf7e8725de27299f3e8d62df
asadaqain <sadaqain@yorku.ca>
2018-07-26 10:47:11 -0400

    Fix SL2 log result view

--- app/controllers/sl2/search_ticket_controller.rb
+++ app/controllers/sl2/search_ticket_controller.rb
@@ -1,13 +1,13 @@
 class Sl2::SearchTicketController < Sl2::AuthorizedBaseController
 
   before_action :load_search_ticket
-  # before_action :check_ticket_status, only: [:edit, :update]
+  before_action :check_ticket_status, only: [:edit, :update]
 
   def show
   end
 
   def edit
-    @work_log = SearchTicket::WorkLog.all
+    @work_log = SearchTicket::WorkLog.new
   end
 
   def update
@@ -46,6 +46,13 @@ class Sl2::SearchTicketController < Sl2::AuthorizedBaseController
   def load_search_ticket
     @ticket = SearchTicket.find(params[:id])
   end
+  
+  def check_ticket_status
+    if @ticket.status != SearchTicket::STATUS_SEARCH_IN_PROGRESS || @ticket.work_logs.size > 0
+      redirect_to sl1_search_ticket_path(@ticket), notice: "Search in progress already."
+    end
+  end
+  
 
   def work_log_params
      params.require(:search_ticket_work_log).permit(:result, :found_location, :note, search_area_ids: [])
