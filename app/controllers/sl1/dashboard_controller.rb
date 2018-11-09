3949c4655f380361469997e95a71e00b4d8f9e14
Taras Danylak <taras@danylak.com>
2018-11-09 08:50:00 -0500

    Fixed assigned to me tickets displaying all tickets assigned to user, regardless of rresolution

--- app/controllers/sl1/dashboard_controller.rb
+++ app/controllers/sl1/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Sl1::DashboardController < Sl1::AuthorizedBaseController
   def show
     @new_tickets = SearchTicket.new_tickets
-    @assigned_tickets = SearchTicket.where(assigned_to_id: current_user.id)
+    @assigned_tickets = SearchTicket.in_progress_tickets.where(assigned_to_id: current_user.id)
   end
 end
--- app/controllers/sl2/dashboard_controller.rb
+++ app/controllers/sl2/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Sl2::DashboardController < Sl2::AuthorizedBaseController
   def show
     @escalated_tickets = SearchTicket.escalated_tickets
-    @assigned_tickets = SearchTicket.where(assigned_to_id: current_user.id)
+    @assigned_tickets = SearchTicket.in_progress_tickets.where(assigned_to_id: current_user.id)
   end
 end
