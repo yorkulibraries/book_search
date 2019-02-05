40d5fbe38e5e70a0e07faad6692f550193756f46
Taras Danylak <taras@danylak.com>
2019-02-05 09:52:58 -0500

    Lots of visual changes

--- app/controllers/manager/employees_controller.rb
+++ app/controllers/manager/employees_controller.rb
@@ -1,5 +1,10 @@
 class Manager::EmployeesController < Manager::AuthorizedBaseController
   def show
     @employee = Employee.find(params[:id])
+
+    @assigned_count = @employee.assigned_tickets.count
+    @resolved_count = @employee.work_logs.resolved_found.count
+    @under_review_count = @employee.work_logs.under_review.count
+    @in_acquisitions_count = @employee.work_logs.in_acquisitions.count
   end
 end
