1e75a88f49a6ced7f2cd403debecea2cb14e0a5e
asadaqain <sadaqain@yorku.ca>
2019-02-08 16:36:57 -0500

    * Add employee CRUD for Admin

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -1,36 +1,35 @@
 class Admin::EmployeesController <  Admin::AuthorizedBaseController
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
 
-  # GET /employees
-  # GET /employees.json
+  # GET /admin/employees
+  # GET /admin/employees.json
   def index
     @employees = Employee.all
   end
 
-  # GET /employees/1
-  # GET /employees/1.json
+  # GET /admin/employees/1
+  # GET /admin/employees/1.json
   def show
   end
 
-  # GET /employees/new
+  # GET /admin/employees/new
   def new
     @employee = Employee.new
   end
 
-  # GET /employees/1/edit
+  # GET /admin/employees/1/edit
   def edit
   end
 
-  # POST /employees
-  # POST /employees.json
+  # POST /admin/employees
+  # POST /admin/employees.json
   def create
     @employee = Employee.new(employee_params)
 
     respond_to do |format|
       if @employee.save
-
         format.html { redirect_to admin_employee_path(@employee), notice: 'Employee was successfully created.' }
-        format.json { render :show, status: :created, location: @employee }
+        format.json { render :show, status: :created, location: admin_employee_path(@employee) }
       else
         format.html { render :new }
         format.json { render json: @employee.errors, status: :unprocessable_entity }
@@ -38,13 +37,13 @@ class Admin::EmployeesController <  Admin::AuthorizedBaseController
     end
   end
 
-  # PATCH/PUT /employees/1
-  # PATCH/PUT /employees/1.json
+  # PATCH/PUT /admin/employees/1
+  # PATCH/PUT /admin/employees/1.json
   def update
     respond_to do |format|
       if @employee.update(employee_params)
         format.html { redirect_to admin_employee_path(@employee), notice: 'Employee was successfully updated.' }
-        format.json { render :show, status: :ok, location: @employee }
+        format.json { render :show, status: :ok, location: admin_employee_path(@employee) }
       else
         format.html { render :edit }
         format.json { render json: @employee.errors, status: :unprocessable_entity }
@@ -52,8 +51,8 @@ class Admin::EmployeesController <  Admin::AuthorizedBaseController
     end
   end
 
-  # DELETE /employees/1
-  # DELETE /employees/1.json
+  # DELETE /admin/employees/1
+  # DELETE /admin/employees/1.json
   def destroy
     @employee.destroy
     respond_to do |format|
@@ -70,6 +69,6 @@ class Admin::EmployeesController <  Admin::AuthorizedBaseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def employee_params
-      params.require(:employee).permit(:name, :role, :login_id, :email, :location_id, :active)
+      params.require(:employee).permit(:name, :role, :email, :login_id, :location_id, :active)
     end
 end
--- app/views/admin/employees/show.json.jbuilder
+++ app/views/admin/employees/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.partial! "admin/employees/employee", employee: @employee
+json.partial! "admin_employees/admin_employee", admin_employee: @employee
