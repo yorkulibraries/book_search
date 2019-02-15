728cd5e26d3eccd445a6283d13952c39a12cd29c
Taras Danylak <taras@danylak.com>
2019-02-15 09:28:13 -0500

    Made location mandatory

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,15 +8,16 @@ class Employee < ApplicationRecord
   ROLE_LEVEL_TWO ="service_level_two"
   ROLES = [ ROLE_MANAGER, ROLE_COORDINATOR, ROLE_LEVEL_ONE, ROLE_LEVEL_TWO ]
 
-  ## VALIDATIONS
-  validates_uniqueness_of :login_id
-  validates_presence_of :login_id, :name, :email, :role
-
   ## RELATIONS
-  belongs_to :location
+  belongs_to :location, optional: false
+
   has_many :tickets_in_location, -> (e) { where(location_id: e.location_id) }, foreign_key: "assigned_to_id", class_name: "SearchTicket"
   has_many :assigned_tickets, foreign_key: "assigned_to_id", class_name: "SearchTicket"
   has_many :work_logs, class_name: "SearchTicket::WorkLog"
+
+  ## VALIDATIONS
+  validates_uniqueness_of :login_id
+  validates_presence_of :login_id, :name, :email, :role, :location_id
 
   ## SCOPES
   scope :by_role, -> { order(:role) }
